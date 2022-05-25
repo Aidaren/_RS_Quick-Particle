1139e8f99eee97b0d81224b87eafbd65b0153753
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-05-25 21:59:02 +0800

    Create Module.lua

---
+++ QuickParticle/Require/Module.lua
@@ -0,0 +1,11 @@
+local Moduls = {
+	
+	["GroundPosition"] = script.Parent.Parent.GroundPosition ,
+	["BezierCurve"] = script.Parent.Parent.BezierCurve ,
+	["SpriteSheet"] = script.Parent.Parent.SpriteSheet ,
+	["Velocity"] = script.Parent.Parent.Velocity ,
+	["Weld"] = script.Parent.Parent.Weld
+	
+}
+
+return Moduls

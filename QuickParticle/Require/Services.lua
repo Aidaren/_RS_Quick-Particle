ec4fcb6a71698e39db663317a91de960d95d7c96
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-05-25 21:59:25 +0800

    Create Services.lua

---
+++ QuickParticle/Require/Services.lua
@@ -0,0 +1,13 @@
+local Services = {
+	
+	["ReplicatedStorage"] = game:GetService("ReplicatedStorage") ,
+	["UserInputService"] = game:GetService("UserInputService") ,
+	["ServerStorage"] = game:GetService("ServerStorage") ,
+	["TweenService"] = game:GetService("TweenService") ,
+	["RunService"] = game:GetService("RunService") ,
+	["Players"] = game:GetService("Players") ,
+	["Debris"] = game:GetService("Debris")
+	
+}
+
+return Services

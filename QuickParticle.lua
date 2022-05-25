a0ad7d114438dd11e671a6e5761e92f6c2d5f50d
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-05-25 21:49:15 +0800

    Create QuickParticle.lua

---
+++ QuickParticle.lua
@@ -0,0 +1,61 @@
+--[[
+Made By Aidaren / 究极挨打人
+
+Version - 0.1.0
+
+Contact Me:
+WeChat: AidarenADR
+Discord: Aidaren#5009
+--]]
+
+--<服务>--
+local Services = require(script.Require.Services)
+
+--<模块>--
+local Modules = require(script.Require.Modules)
+
+--<事件>--
+local QuickParticle = {}
+
+function QuickParticle.CreateWeld(Part0 , Part1 , WeldName , C0)
+	
+	local Parameter = {Part0 , Part1 , WeldName , C0}
+	Modules.Weld.CreateWeld(Parameter)
+	
+end
+
+function QuickParticle.BreakWeld(WeldParent , WeldName , DestoryDelay)
+	
+	local Parameter = {WeldParent , WeldName, DestoryDelay}
+	Modules.Weld.BreakWeld(Parameter)
+	
+end
+
+function QuickParticle.GetGroundPosition(Part , IgnoreList)
+	
+	local Parameter = {Part , IgnoreList}
+	Modules.GroundPosition(Parameter)
+	
+end
+
+function QuickParticle.LoopSpriteSheet(ParticlePreset , SpriteSheet , Parent , EmitCount , FrameTime , DestoryDelay)
+	
+	local Parameter = {ParticlePreset , SpriteSheet , Parent , EmitCount , FrameTime , DestoryDelay}
+	Modules.SpriteSheet(Parameter)
+	
+end
+
+function QuickParticle.AddVelocity(Parent , Strength , DestoryDelay)
+	
+	local Parameter = {Parent , Strength , DestoryDelay}
+	Modules.Velocity(Parameter)
+	
+end
+
+function QuickParticle.BezierCurve(Mode:string , Parameter)
+	
+	Modules.BezierCurve:FindFirstChild(Mode)(Parameter)
+	
+end
+
+return QuickParticle

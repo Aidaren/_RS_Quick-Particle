58345febc886b989b7dd0e49394bcc4ace00b8db
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-05-25 21:52:38 +0800

    Create Velocity.lua

---
+++ QuickParticle/Velocity.lua
@@ -0,0 +1,17 @@
+return function(Parameter)
+	
+	local Parent = Parameter[1]
+	local Strength = Parameter[2] or 1
+	local DestoryDelay = Parameter[3] or 0.25
+	
+	local Velocity = Instance.new('BodyVelocity')
+	Velocity.Name = 'Velocity'
+	Velocity.MaxForce = Vector3.new(1, 1, 1) * math.huge
+	Velocity.Velocity = Strength
+	Velocity.Parent = Parent
+	
+	game:GetService('Debris'):AddItem(Velocity , DestoryDelay)
+	
+	return Velocity
+	
+end

f19dc202928306e7a6d7ff2008d5114e8d7988f2
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-08-22 15:59:02 +0800

    Update QuickParticle.lua

--- QuickParticle.lua
+++ QuickParticle.lua
@@ -1,7 +1,7 @@
 --[[
 Made By Aidaren / 究极挨打人
 
-Version - 0.1.3
+Version - 0.2.2
 
 Contact Me:
 WeChat: AidarenADR
@@ -29,6 +29,27 @@ function QuickParticle.BreakWeld(WeldParent , WeldName , DestoryDelay)
 	local Parameter = {WeldParent , WeldName, DestoryDelay}
 	Modules.Weld.BreakWeld(Parameter)
 	
+end
+
+function QuickParticle.EmitAll(Parent , EmitDelay)
+	
+	local Parameter = {Parent , EmitDelay}
+	Modules.Operate.EmitAll(Parameter)
+	
+end
+
+function QuickParticle.EnableAll(Parent , EnableDelay)
+
+	local Parameter = {Parent , EnableDelay}
+	Modules.Operate.EnableAll(Parameter)
+
+end
+
+function QuickParticle.DisableAll(Parent , DisableDelay)
+
+	local Parameter = {Parent , DisableDelay}
+	Modules.Operate.DisableAll(Parameter)
+
 end
 
 function QuickParticle.GetGroundPosition(Part , IgnoreList)

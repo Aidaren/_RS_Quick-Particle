8646ad8936cc844dd5867bca7a9c38bde97f864c
Aidaren <78560731+Aidaren@users.noreply.github.com>
2022-08-22 16:01:22 +0800

    Update Operate.lua

--- QuickParticle/Operate.lua
+++ QuickParticle/Operate.lua
@@ -33,4 +33,46 @@ function Operate.EmitAll(Parameter)
 	
 end
 
+function Operate.EnableAll(Parameter)
+	
+	local Parent = Parameter[1]
+	local EnableDelay = Parameter[2] or task.wait()
+	
+	for Index , Value in Parent:GetDescendants() do
+		
+		task.delay(EnableDelay , function()
+			
+			if Value:IsA("ParticleEmitter") or Value:IsA("Beam") then
+				
+				Value.Enabled = true
+				
+			end
+			
+		end)
+		
+	end
+	
+end
+
+function Operate.DisableAll(Parameter)
+	
+	local Parent = Parameter[1]
+	local DisableDelay = Parameter[2] or task.wait()
+	
+	for Index , Value in Parent:GetDescendants() do
+		
+		task.delay(DisableDelay , function()
+			
+			if Value:IsA("ParticleEmitter") or Value:IsA("Beam") then
+				
+				Value.Enabled = false
+				
+			end
+			
+		end)
+		
+	end
+	
+end
+
 return Operate
